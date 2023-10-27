479017920f342733373c5d8ad9b2b2c97f5fd3bc
Dyego Maas <dyego.maas@ambevtech.com.br>
2023-10-27 13:30:32 -0300

    Criando estrutura.

---
+++ DojoIoc/README.md
@@ -0,0 +1,12 @@
+﻿# DOJO - IoC Containers
+
+## Exercícios
+
+### 1. Registrando tudo como "transient"
+
+### 2. Registrando tudo como "singleton"
+
+### 3. Registrando tudo como "scoped"
+
+
+### 3.1. Criando escopos
--- DojoIoc/WebApplication/Program.cs
+++ DojoIoc/WebApplication/Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureServices(serviceCollection =>
+{
+    serviceCollection.AddSingleton<IMyInterface>(new MyClass());
+});
+
 var app = builder.Build();
+
+app.Services.GetService<IMyInterface>();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+public interface IMyInterface
+{
+}
+
+class MyClass : IMyInterface
+{
+}
